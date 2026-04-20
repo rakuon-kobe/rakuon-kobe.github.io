4c611f310ae1a9db4796a0d732c14710f10cf07b
rakuon-kobe <rakuon.kobe.u@gmail.com>
2026-04-20 09:29:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -28,13 +28,13 @@ const EVENT_DETAILS = {
     picnic: { // ★タコパと同じ設定で作成
         title: "ピクニック 🥪",
         gasName: "ピクニック", 
-        date: "5月12日(火) 12:00〜15:00",
+        date: "5月2日(火) 12:00〜15:00",
         location: "三ノ宮駅近くの公園 ※詳細は予約確定後にお送りします",
         fee: "無料",
         description: "公園で軽食を食べながらみんなでおしゃべりしましょう！おひとり様でも大丈夫です😊",
         belongings: "特になし（手ぶらでOK！）",
-        reserveStart: "2026/04/22 10:00",
-        eventEnd: "2026/05/02 14:00",
+        reserveStart: "2026/04/22 12:00",
+        eventEnd: "2026/05/02 11:00",
         theme: "#F5B82E",
         glow: "rgba(245, 184, 46, 0.3)"
     },
@@ -59,7 +59,7 @@ const EVENT_DETAILS = {
         fee: "無料",
         description: "楽器を実際に触ってみる体験会です。未経験者・初心者大歓迎！先輩が丁寧に教えてくれます🎸",
         belongings: "特になし（自分の楽器を持ってきていただいても大丈夫です！）",
-        reserveStart: "2026/04/22 12:00",
+        reserveStart: "2026/04/26 12:00",
         eventEnd: "2026/05/09 10:00",
         theme: "#FFD131",
         glow: "rgba(255, 209, 49, 0.3)"
